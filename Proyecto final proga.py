a108ad5cf75bc5be83a0928d8b05d682998d49b7
FelipeM97 <86996970+FelipeM97@users.noreply.github.com>
2021-07-06 19:03:08 -0400

    Update Proyecto final proga.py

--- Proyecto final proga.py
+++ Proyecto final proga.py
@@ -10,10 +10,34 @@ mensaje=input("Ingrese un mensaje: ")
 
 mensajep=mensaje.lower()
 
-print("su traduccion respectiva es: ",end=" ")
+
+print("""
+1 - Region de Arica y Parinacota
+2 - Region de Tarapacá
+3 - Region de Antofagasta
+4 - Region de Atacama
+5 - Region de Coquimbo
+6 - Region de Valparaiso
+7 - Region Metropolitana
+8 - Region del Libertador General Bernardo O’Higgins
+9 - Region del Maule
+10 - Region del Nuble
+11 - Region del Biobio
+12 - Region de La Araucania
+13 - Region deLos Rios
+14 - Region deLos Lagos
+15 - Region de Aysen
+16 - Region de Magallanes y la Antartica""")
+
+
+seleccion = input()
+print("Su seleccion es:,"seleccion)
 
 
 
+
+
+print("Su Region seleccionada es:,")
 
 
 lalista=[] 
