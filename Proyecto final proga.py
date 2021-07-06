290ce09026f8c313541634246662e85a9706bebd
FelipeM97 <felipe.mendezb@alumnos.uv.cl>
2021-07-06 01:17:26 -0400

    Hola2

--- Proyecto final proga.py
+++ Proyecto final proga.py
@@ -1,9 +1,19 @@
-###print("Bienvenido, en este programa podra seleccionar y ver dependiendo de la region,comuna, los casos de una fecha en especifico que usted desee")
-import matplotlib.pyplot as plt
-
-ejex=[4,8,13,17,20]
-ejey=[54,67,98,78,45]
-plt.plot(ejex,ejey)
-plt.show()
-
-print("Wena")
+print("Bienvenido, en este programa podra seleccionar y ver dependiendo de la region,comuna, los casos de una fecha en especifico que usted desee")
+print("escoja una comuna de esta lista colocando el respectivo numero que le corresponde")
+print("""
+1 - Region de Arica y Parinacota
+2 - Region de Tarapacá
+3 - Region de Antofagasta
+4 - Region de Atacama
+5 - Region de Coquimbo
+6 - Region de Valparaiso
+7 - Region Metropolitana
+8 - Region del Libertador General Bernardo O’Higgins
+9 - Region del Maule
+10 - Region del Nuble
+11 - Region del Biobio
+12 - Region de La Araucania
+13 - Region deLos Rios
+14 - Region deLos Lagos
+15 - Region de Aysen
+16 - Region de Magallanes y la Antartica""")
