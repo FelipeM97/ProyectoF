39feb2b5acb87018522521a9d5c10e5221141100
FelipeM97 <felipe.mendezb@alumnos.uv.cl>
2021-07-06 00:23:02 -0400

    first commit

---
+++ Proyecto final proga.py
@@ -0,0 +1,7 @@
+###print("Bienvenido, en este programa podra seleccionar y ver dependiendo de la region,comuna, los casos de una fecha en especifico que usted desee")
+import matplotlib.pyplot as plt
+
+ejex=[4,8,13,17,20]
+ejey=[54,67,98,78,45]
+plt.plot(ejex,ejey)
+plt.show()
